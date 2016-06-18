fface71dbfb9344ba57c98120ca301376dea1f21
Simon Gene Gottlieb <gene@staubsaal.de>
2016-06-18 13:35:10 +0200

    adding missing id increment for on_load register

--- src/commonOptions/Singleton.cpp
+++ src/commonOptions/Singleton.cpp
@@ -12,11 +12,13 @@ auto Singleton::register_on_before_save(std::function<void()> _callback) -> Call
 	std::unique_lock<std::mutex> lock(mMutex);
 
 	id += 1;
+
+	auto copyID = id;
 	mCallbacksOnBeforeSave.emplace_back(std::make_pair(id, _callback));
 	return CallbackHandler { new CallbackOnExitHandler ([=] {
 		std::unique_lock<std::mutex> lock(mMutex);
 		for (auto iter = mCallbacksOnBeforeSave.begin(); iter != mCallbacksOnBeforeSave.end(); ++iter) {
-			if (iter->first == id) {
+			if (iter->first == copyID) {
 				mCallbacksOnBeforeSave.erase(iter);
 				return;
 			}
@@ -28,11 +30,13 @@ auto Singleton::register_on_save(std::function<void()> _callback) -> CallbackHan
 	std::unique_lock<std::mutex> lock(mMutex);
 
 	id += 1;
+
+	auto copyID = id;
 	mCallbacksOnSave.emplace_back(std::make_pair(id, _callback));
 	return CallbackHandler { new CallbackOnExitHandler ([=] {
 		std::unique_lock<std::mutex> lock(mMutex);
 		for (auto iter = mCallbacksOnSave.begin(); iter != mCallbacksOnSave.end(); ++iter) {
-			if (iter->first == id) {
+			if (iter->first == copyID) {
 				mCallbacksOnSave.erase(iter);
 				return;
 			}
@@ -42,11 +46,14 @@ auto Singleton::register_on_save(std::function<void()> _callback) -> CallbackHan
 auto Singleton::register_on_load(std::function<void()> _callback) -> CallbackHandler {
 	std::unique_lock<std::mutex> lock(mMutex);
 
+	id += 1;
+
+	auto copyID = id;
 	mCallbacksOnLoad.emplace_back(std::make_pair(id, _callback));
 	return CallbackHandler { new CallbackOnExitHandler ([=] {
 		std::unique_lock<std::mutex> lock(mMutex);
 		for (auto iter = mCallbacksOnLoad.begin(); iter != mCallbacksOnLoad.end(); ++iter) {
-			if (iter->first == id) {
+			if (iter->first == copyID) {
 				mCallbacksOnLoad.erase(iter);
 				return;
 			}
